4a4294b7d3be9ce57db6a6b22726073ac90dd378
Jean-Sebastien Paquet <paquet@cadwork.ca>
2024-04-23 11:32:45 -0400

    Initial commit.

---
+++ EnvelopeAssembly.py
@@ -0,0 +1,50 @@
+# Copyright 2024 Cadwork Informatique Inc.
+# All rights reserved.
+# This file is part of EnvelopeAssembly,
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+import element_controller as ec
+import utility_controller as uc
+import attribute_controller as ac
+from cadwork.element_grouping_type import element_grouping_type
+
+
+def get_grouping_attribute(element):
+    """
+    Returns the grouping attribute of a given element.
+
+    Args:
+        element: The element to get the grouping attribute for.
+
+    Returns: The grouping attribute.
+
+    """
+    if ac.get_element_grouping_type() == element_grouping_type.group:
+        return ac.get_group(element)
+    elif ac.get_element_grouping_type() == element_grouping_type.subgroup:
+        return ac.get_subgroup(element)
+
+    raise RuntimeError('unknown element grouping type')
+
+
+if __name__ == "__main__":
+    active_elements = ec.get_active_identifiable_element_ids()
+
+    active_envelopes = {}
+
+    try:
+        for active_element in active_elements:
+            if ac.is_envelope(active_element):
+                grouping_attribute = get_grouping_attribute(active_element)
+                active_envelopes[grouping_attribute] = {'assembly': ac.get_assembly_number(active_element), 'elements': []}
+
+        for active_element in active_elements:
+            if not ac.is_envelope(active_element):
+                grouping_attribute = get_grouping_attribute(active_element)
+
+                if grouping_attribute in active_envelopes:
+                    ac.set_assembly_number([active_element], active_envelopes[grouping_attribute]['assembly'])
+
+    except RuntimeError:
+        uc.print_error('failed to get envelopes')
---
+++ README.md
@@ -0,0 +1,18 @@
+![Logo](https://filehost.cadwork.ca/cadwork_logo.png)
+
+# EnvelopeAssembly
+
+Assigns assembly numbers from envelopes to elements grouped within.
+
+## Installation
+
+Clone in `API.x64` folder of 3D.
+
+```bash
+cd 'c:\users\public\documents\cadwork\userprofil_30\API.x64'
+git clone https://github.com/cwapi3d/EnvelopeAssembly.git
+```
+
+## Authors
+
+- [@jspaquet](https://www.github.com/jspaquet)
